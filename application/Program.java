ee4091c0071621e84b34e59af8e899c944475297
Bruno Gomes <gomes_bruno97@hotmail.com>
2022-04-15 07:43:22 -0300

    Criado Program principal com os primeiros dados e já instânciado

--- application/Program.java
+++ application/Program.java
@@ -1,5 +1,26 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Departament;
+import entities.Worker;
+import entities_enums.WorkerLevel;
 public class Program {
-    
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter department's name: ");
+        String departmentName = input.nextLine();
+        System.out.println("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = input.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = input.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = input.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerName), baseSalary, new Departament(departmentName));
+    }
 }
