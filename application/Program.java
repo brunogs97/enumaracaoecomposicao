c36a09c9afde8ce6b5198815e93c3de682b15613
Bruno Gomes <gomes_bruno97@hotmail.com>
2022-04-15 08:03:14 -0300

    Criado os dados de contrato e adicionado na Lista de contratos

--- application/Program.java
+++ application/Program.java
@@ -1,16 +1,21 @@
 package application;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Departament;
+import entities.HourContract;
 import entities.Worker;
 import entities_enums.WorkerLevel;
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
+        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/YYYY");
 
         System.out.print("Enter department's name: ");
         String departmentName = input.nextLine();
@@ -22,5 +27,20 @@ public class Program {
         System.out.print("Base salary: ");
         double baseSalary = input.nextDouble();
         Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerName), baseSalary, new Departament(departmentName));
+
+        System.out.print("How many contracts to this worker? ");
+        int n = input.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println("Enter contract #" + i + " data:");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate = sfd.parse(input.next());
+            System.out.print("Value per hour: ");
+            double valuePerHour = input.nextDouble();
+            System.out.println("Duration (hours): ");
+            int hours = input.nextInt();
+            HourContract contracts = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contracts);
+        }
     }
 }
