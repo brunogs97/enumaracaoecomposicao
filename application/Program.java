94154c1a5046ca13d9b35af91d943a4247f8b226
Bruno Gomes <gomes_bruno97@hotmail.com>
2022-04-15 19:54:31 -0300

    Finalizado o projeto

--- application/Program.java
+++ application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
-        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/YYYY");
+        SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.print("Enter department's name: ");
         String departmentName = input.nextLine();
@@ -39,8 +39,17 @@ public class Program {
             double valuePerHour = input.nextDouble();
             System.out.println("Duration (hours): ");
             int hours = input.nextInt();
-            HourContract contracts = new HourContract(contractDate, valuePerHour, hours);
-            worker.addContract(contracts);
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
         }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+        String monthAndYear = input.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+        System.out.print("Name: " + worker.getName());
+        System.out.print("Depertment: " + worker.getDepartament().getName());
+        System.out.print("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
     }
 }
